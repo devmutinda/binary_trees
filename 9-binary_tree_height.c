1cf87894d2a439e15d282cad5b55ebae84c64ad1
devmutinda <davidkymutinda@gmail.com>
2022-08-26 11:13:54 +0000

    Made Betty compliant

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (!tree->left && !tree->right)
-		return(0);
+		return (0);
 
 	left = binary_tree_height(tree->left) + 1;
 	right = binary_tree_height(tree->right) + 1;
