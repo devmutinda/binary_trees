1204edfb7f61e45de215d4d9b01368e333feb031
devmutinda <davidkymutinda@gmail.com>
2022-08-26 11:09:57 +0000

    Removed redundancy

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,8 +11,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right)
-		return (0);
 	left = binary_tree_height(tree->left) + 1;
 	right = binary_tree_height(tree->right) + 1;
 
