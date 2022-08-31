1f9e81393ab1e73ee02b83ad2c4af5eefd8c8666
devmutinda <davidkymutinda@gmail.com>
2022-08-31 05:58:21 +0000

    Fixed error

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -18,7 +18,10 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 	parent->parent = temp;
 	tree->parent = parent;
 	if (hold)
+	{
+		hold->parent = tree;
 		tree->left = hold;
+	}
 	else
 		tree->left = NULL;
 
