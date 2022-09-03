21ccf11262ef7362b41f7cb8fcd9d97c9aafb9ce
devmutinda <davidkymutinda@gmail.com>
2022-09-03 09:07:40 +0000

    Modified

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -129,9 +129,13 @@ int heap_extract(heap_t **root)
 
 	if (!(*root))
 		return (0);
-
 	value = (*root)->n;
-	if ((*root)->left || (*root)->right)
+	if (!(*root)->left && !(*root)->right)
+	{
+		binary_tree_delete(*root);
+		*root = NULL;
+	}
+	else
 	{
 		height = get_height(*root);
 		get_new_node(*root, height, &new);
@@ -153,15 +157,11 @@ int heap_extract(heap_t **root)
 			new->left->parent = new;
 		if (new->right)
 			new->right->parent = new;
-		free(*root);
+		(*root)->left = NULL;
+		(*root)->right = NULL;
+		binary_tree_delete(*root);
 		new = swap_elements(new);
 		*root = new;
 	}
-	else
-	{
-		free(*root);
-	}
-
-
 	return (value);
 }
